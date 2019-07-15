1ea79649f6ee8c8a227bba753ea7cd2467c162f6
Roxanne Reyes <waetechsolutions@gmail.com>
2019-07-15 09:19:25 -0400

    Added routing

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,13 +4,18 @@
 
     <form @submit.prevent="addSkill">
     <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'" name="skill">
-    <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill')}}</p>
     
-   
+    <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+     <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill')}}</p>
+    </transition>
     </form>
 
+    
     <ul>
+      <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
       <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+      <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+      </transition-group>
     </ul>
 
     <p>These are the skills that you possess.</p>
@@ -39,8 +44,10 @@ export default {
          } else {
            console.log('Not valid');
          }
-       })
-      
+       })      
+      },
+      remove(id) {
+        this.skills.splice(id, 1);
       }
    }
 }
@@ -50,7 +57,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- 
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/js/fontawesome.min.js";
+
  .holder {
    background: #fff;
  }
@@ -95,5 +104,24 @@ input {
   display: inline-block;
   padding: 5px;
   margin-top: -20px;
+}
+
+.alert-in-enter-active {
+  animation: bounce-in .5s;
+}
+.alert-in-leave-active {
+  animation: bounce-in .5s reverse;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
